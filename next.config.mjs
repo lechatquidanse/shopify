835bc3ba5b5ed2505fedb27b7fab35545d0c346b
lechatquidanse <stephane.elmanouni@gmail.com>
2025-11-16 19:25:07 +0100

    deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/shopify/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/shopify/' : '',
 }
 
 export default nextConfig
